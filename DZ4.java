1860e0b4e7363d409695e9ea1b28cdfccfee0769
Alexey Silaenkov <silaenckov2014@yandex.ru>
2023-06-27 09:44:41 +0300

    add dz4

---
+++ DZ4.java
@@ -0,0 +1,14 @@
+public class DZ4 {
+    public static void main(String[] args) {
+        BinTree<Integer> tree = new BinTree<>();
+        tree.add(10);       
+        tree.add(3);
+        tree.add(5);
+        tree.add(8);
+        tree.add(245);
+        tree.add(436);
+        tree.add(0);
+
+        tree.print();
+    }
+}
